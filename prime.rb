00da86cc2ee00e230c039babefada346f00bf17c
Jeremy Lenz <jeremylenz@gmail.com>
2017-05-22 17:54:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+# require 'pry'
+def prime?(test_number)
+return false if test_number <= 1
+is_prime = true
+numbers_to_test = (2...test_number).to_a
+numbers_to_test.each do |possible_multiple|
+
+  if test_number % possible_multiple == 0
+    is_prime = false
+
+    break
+  end
+end
+is_prime
+end
